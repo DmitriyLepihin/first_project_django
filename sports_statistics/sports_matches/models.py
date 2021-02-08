c718daf9d40a761b640c9604d236165bd3404d6c
DmitriyLepihin <dmitrijlepihin1991@gmail.com>
2021-02-08 15:49:07 +0300

    add attribute league (model MatchResults)

--- sports_statistics/sports_matches/models.py
+++ sports_statistics/sports_matches/models.py
@@ -3,9 +3,14 @@ from django.db import models
 
 class MatchResults(models.Model):
     type_sport = models.CharField(max_length=150, null=True)
+    league = models.CharField(max_length=150, null=True)
     team_one = models.CharField(max_length=350, null=True)
     team_two = models.CharField(max_length=350, null=True)
     score_one = models.IntegerField()
     score_two = models.IntegerField()
     date_match = models.DateField()
     match_details = models.CharField(max_length=800, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.type_sport} - league {self.league}: {self.team_one} {self.score_one}:\
+{self.score_two} {self.team_two}"
